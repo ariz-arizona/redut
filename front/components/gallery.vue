da066282e82e4e836cf97dc716337c83ad3e199e
aarizona <ar.ariz.arizona@gmail.com>
2025-03-23 22:34:18 +0300

    gallery fade

--- front/components/gallery.vue
+++ front/components/gallery.vue
@@ -25,6 +25,7 @@ const swiperOptions: SwiperOptions = {
     freeMode: true,
     slidesOffsetBefore: (window.innerWidth - 1024) * 0.5,
     effect: 'fade', 
+    spaceBetween: 24,
 };
 
 // Ссылка на Swiper для управления (например, пауза/воспроизведение)
@@ -33,7 +34,7 @@ const swiperRef = ref(null);
 <template>
     <swiper ref="swiperRef" v-bind="(swiperOptions as any)" class="w-screen">
         <swiper-slide v-for="(slide, index) in props.slides" :key="index">
-            <div class="bg-no-repeat bg-cover bg-center h-96 rounded-2xl overflow-hidden flex justify-start align-bottom p-4 bg-origin-content"
+            <div class="bg-no-repeat bg-cover bg-center h-96 rounded-2xl overflow-hidden flex justify-start align-bottom p-4"
                 :style="[{ backgroundImage: `url(${imgBase}/${slide.image})` }]">
                 <div class="self-end">
                     <h3 class="font-bold text-2xl">{{ slide.title }}</h3>

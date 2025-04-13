1d9a3012cd42068af3d165fa08597a3a99950907
aarizona <ar.ariz.arizona@gmail.com>
2025-04-13 19:57:01 +0300

    banner txt

--- front/components/slider/item.vue
+++ front/components/slider/item.vue
@@ -29,14 +29,10 @@ const imgBase = config.public.imgBase
         </div>
       </div>
     </div>
-    <div v-if="add_text">
-      <div class="absolute top-1/2 right-2 left-2">
-        <div class="container relative grid grid-cols-1 md:grid-cols-3">
-          <div class="col-span-1 col-start-3">
-            <div class="bg-midnight-950 text-white p-8 prose prose-invert basetext -translate-y-1/2">
-              <span v-html="add_text" />
-            </div>
-          </div>
+    <div class="container relative grid grid-cols-1 md:grid-cols-3 h-full items-center" v-if="add_text">
+      <div class="col-span-1 col-start-3">
+        <div class="bg-midnight-950 text-white p-8 prose prose-invert basetext">
+          <span v-html="add_text" />
         </div>
       </div>
     </div>

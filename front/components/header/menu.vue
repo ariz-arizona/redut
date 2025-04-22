b373282793b509e5d327eb6cf186127d9891c047
aarizona <ar.ariz.arizona@gmail.com>
2025-04-22 16:45:09 +0300

    smooth scroll

--- front/components/header/menu.vue
+++ front/components/header/menu.vue
@@ -1,8 +1,27 @@
+<script setup lang="ts">
+import { useScroll } from '@vueuse/core';
+
+const scrollToFeedback = () => {
+    // Находим элемент с id="feedback"
+    const feedbackElement = document.getElementById('feedback');
+
+    if (feedbackElement) {
+        // Используем useSmoothScroll для плавной прокрутки
+        const { x, y } = useScroll(window, { behavior: 'smooth' })
+
+        y.value = feedbackElement.getBoundingClientRect().top
+    } else {
+        navigateTo('contacts')
+    }
+
+}
+</script>
+
 <template>
-    <div class="menubtn">
-        <Icon name="mdi:menu" class="text-sandal-500 text-xl" />
+    <NuxtLink to="#feedback" class="menubtn" @click.prevent="scrollToFeedback">
+        <Icon name="mdi:chevron-down" class="text-sandal-500 text-xl" />
         <span>
-            Меню
+            Обратная связь
         </span>
-    </div>
+    </NuxtLink>
 </template>

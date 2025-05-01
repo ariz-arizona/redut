51db424e6afdb093e80ffe5587eafd0e8d0bc5e4
aarizona <ar.ariz.arizona@gmail.com>
2025-05-01 13:13:02 +0300

    arrow

--- front/components/block/category.vue
+++ front/components/block/category.vue
@@ -24,9 +24,13 @@ onMounted(() => { loadPages() })
 <template>
     <div :id="block.slug">
         <div class="container p-4 grid grid-cols-1 xl:grid-cols-[1fr_2fr] gap-4 mb-8">
-            <div class="col-span-1">
+            <div class="col-span-1 flex items-end justify-center">
                 <div class="text-secondary-400 text-7xl font-bleu uppercase sideways-writing-lr text-left indent-20"
                     v-html="block.title || block.category.title" />
+                <NuxtLink :to="`cat/${block.category.slug}`"
+                    class=" flex items-center justify-center leading-none size-24 rounded-full border border-secondary-400">
+                    <Icon name="mdi:arrow-right" class="w-3/4 h-3/4 text-secondary-200" />
+                </NuxtLink>
             </div>
             <div class="col-span-1">
                 <template v-for="item in pages">

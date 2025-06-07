3e925ea476c83d94f65b6e67de773efa0529e7ae
aarizona <ar.ariz.arizona@gmail.com>
2025-06-07 22:47:47 +0300

    png

--- front/components/block/full_image.vue
+++ front/components/block/full_image.vue
@@ -10,23 +10,32 @@ const onImageLoad = (event: Event) => {
         aspectRatio.value = (img.naturalWidth / img.naturalHeight).toString()
     }
 }
+const img_bg = props.block.images[0]
+const img_add = props.block.images[1]
 </script>
 <template>
     <BlockWrapper :block="block">
         <div class="bg-no-repeat bg-cover min-h-[50vh] max-h-[83vh] w-full p-4 relative pointer-events-none"
             :class="[{ '-mt-[33vh]': block.link || block.external_link }]" :style="{
-                backgroundImage: createBgWithGrad(
-                    `${imgBase}/${block.images[0].image}`,
+                backgroundImage: img_bg ? createBgWithGrad(
+                    `${imgBase}/${img_bg.image}`,
                     'var(--image-overlay-color)',
                     '0',
                     '0',
-                ),
+                ) : '',
                 aspectRatio: aspectRatio,
                 backgroundPositionY: '75%'
-            }" v-if="block.images.length">
+            }">
             <NuxtImg @load="onImageLoad" :src="`${imgBase}/${block.images[0].image}`" class="invisible hidden" />
 
-            <div class="container pt-16 z-10 relative h-full flex items-center">
+            <div class="container pt-16 z-10 relative h-full flex items-center bg-no-repeat bg-left-bottom" :style="{
+                backgroundImage: img_add ? createBgWithGrad(
+                    `${imgBase}/${img_add.image}`,
+                    'var(--image-overlay-color)',
+                    '0',
+                    '0',
+                ) : ''
+            }">
                 <div class="grid grid-cols-6 gap-y-10">
                     <div class="col-span-full xl:col-span-3 xl:col-start-3">
                         <div class="text-7xl md:text-[6.5vh] font-bleu uppercase leading-tight">

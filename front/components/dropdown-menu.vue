27e828a3ef49d3caf8fa7d61bfe82548005938f5
aarizona <ar.ariz.arizona@gmail.com>
2025-06-29 09:54:27 +0300

    menu

--- front/components/dropdown-menu.vue
+++ front/components/dropdown-menu.vue
@@ -7,6 +7,7 @@ const props = defineProps({
 
 const isOpen = ref(false)
 const { items: menuItems } = useMenuPages()
+const { settings } = useSiteSettings()
 
 const toggleMenu = () => {
     if (!menuItems.value.length) return
@@ -35,16 +36,19 @@ const scrollToAnchor = (e: PointerEvent) => {
 <template>
     <div class="relative inline-block text-left">
         <!-- Кнопка меню -->
-        <NuxtLink class="menubtn" @click.prevent="toggleMenu">
+        <NuxtLink class="menubtn" @click.prevent="toggleMenu" v-if="menuItems.length">
             <Icon name="mdi:chevron-down" class="text-secondary-500 text-xl" v-if="menuItems.length" />
             <span>{{ title }}</span>
+        </NuxtLink>
+        <NuxtLink class="menubtn" to='/' v-else>
+            <span>{{ settings?.name }}</span>
         </NuxtLink>
 
         <!-- Выпадающий список -->
         <div v-show="isOpen && menuItems.length"
             class="origin-bottom-right absolute left-0 top-full mt-2 w-56 !rounded-[2rem] z-10 menubtn bg-primary-950/70 flex-col items-start">
-            <div class="py-1 basetext  w-full text-white" role="menu" aria-orientation="vertical" aria-labelledby="options-menu"
-                v-for="item in menuItems">
+            <div class="py-1 basetext  w-full text-white" role="menu" aria-orientation="vertical"
+                aria-labelledby="options-menu" v-for="item in menuItems">
                 <NuxtLink :key="item.id" :href="`#${item.slug}`" @click.prevent="scrollToAnchor" class="text-xs"
                     role="menuitem">
                     {{ item.title }}
